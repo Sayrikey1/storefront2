d1bc907d8a5a501ac6d66499d1cf5bd3763ae044
Sayrikey1 <kamalseriki49@gmail.com>
2023-04-30 01:10:24 +0100

    celery

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,8 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 from django.shortcuts import render
 from templated_mail.mail import BaseEmailMessage
 
-# def say_hello(request):
+# def say_hello(request): 
 #     try:
 #         send_mail('subject', 'the message', 'octavedev01@gmail.com', ['kamalseriki49@gmail.com'])
 #     except BadHeaderError:
@@ -12,8 +12,14 @@ from templated_mail.mail import BaseEmailMessage
 def say_hello(request):
     try:
         
-        message = BaseEmailMessage()
-
+        message = BaseEmailMessage(
+            template_name = 'emails/hello.html',
+            context = {'name': 'Kamal'}
+        ) 
+        message.send(['octavedev01@gmail.com'])
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
+
+
+# docker run -d -p 6379:6379 redis
